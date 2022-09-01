e4af7ccf28fd71ba16f219708149b47713b4a9e5
Luke Prosser <lukesprosser@gmail.com>
2022-09-01 21:06:27 +0100

    add next links in header

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,14 @@
 import { ReactElement, useState } from 'react';
+import Link from 'next/link';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
 
 export default function Header(): ReactElement {
   const [showMobileNav, setShowMobileNav] = useState(false);
   return (
     <nav className='flex flex-wrap items-center justify-between w-full px-4 py-4 text-lg text-slate-50 bg-slate-900 md:py-0'>
-      <a href='#'>
-        <span className='text-xl font-semibold text-slate-50'>WatchStop</span>
-      </a>
+      <Link href='/'>
+        <a className='text-xl font-semibold text-slate-50'>WatchStop</a>
+      </Link>
       {showMobileNav ? (
         <XMarkIcon
           className='block w-6 h-6 cursor-pointer md:hidden'
@@ -29,19 +30,14 @@ export default function Header(): ReactElement {
       >
         <ul className='pt-4 text-base text-slate-200 md:flex md:justify-between md:pt-0'>
           <li>
-            <a className='block py-2 md:p-4 hover:text-slate-50' href='#'>
-              Link 1
-            </a>
+            <Link href='/cart'>
+              <a className='block py-2 md:p-4 hover:text-slate-50'>Cart</a>
+            </Link>
           </li>
           <li>
-            <a className='block py-2 md:p-4 hover:text-slate-50' href='#'>
-              Link 2
-            </a>
-          </li>
-          <li>
-            <a className='block py-2 md:p-4 hover:text-slate-50' href='#'>
-              Link 3
-            </a>
+            <Link href='/login'>
+              <a className='block py-2 md:p-4 hover:text-slate-50'>Login</a>
+            </Link>
           </li>
         </ul>
       </div>
