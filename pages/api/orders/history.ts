c9ca29a6f814f619dbabb6d54e70ee2ab8ebbbba
Luke Prosser <lukesprosser@gmail.com>
2022-10-08 16:19:05 +0100

    create order history endpoint

---
+++ pages/api/orders/history.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import { Types } from 'mongoose';
+import Order from '../../../models/Order';
+import { isAuthenticated } from '../../../utils/auth';
+import db from '../../../utils/db';
+
+type IUser = {
+  _id: Types.ObjectId;
+};
+
+interface IGetUserAuthInfoRequest extends NextApiRequest {
+  user: IUser;
+}
+
+const handler = nc<IGetUserAuthInfoRequest, NextApiResponse>({
+  onError: async (err, req, res, next) => {
+    await db.disconnect();
+    res.status(500).send({ message: err.toString() });
+  },
+});
+
+// Middleware to check authorisation
+handler.use(isAuthenticated);
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const orders = await Order.find({ user: req.user._id });
+  await db.disconnect();
+  res.send(orders);
+});
+
+export default handler;
