f9a0d57c60aa9f6b6c1abf52497a3228317ba554
Luke Prosser <lukesprosser@gmail.com>
2022-10-15 06:24:55 +0100

    create order deliver endpoint

---
+++ pages/api/orders/[id]/deliver.ts
@@ -0,0 +1,35 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import Order from '../../../../models/Order';
+import { isAdministrator, isAuthenticated } from '../../../../utils/auth';
+import db from '../../../../utils/db';
+
+const handler = nc<NextApiRequest, NextApiResponse>({
+  onError: async (err, req, res, next) => {
+    await db.disconnect();
+    res.status(500).send({ message: err.toString() });
+  },
+});
+
+// Middleware to check authorisation
+handler.use(isAuthenticated, isAdministrator);
+
+handler.put(async (req, res) => {
+  await db.connect();
+  const order = await Order.findById(req.query.id);
+  if (order) {
+    order.delivered = true;
+    order.deliveredAt = new Date();
+    const deliveredOrder = await order.save();
+    await db.disconnect();
+    res.send({
+      message: 'Order status updated successfully.',
+      order: deliveredOrder,
+    });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: 'Order not found.' });
+  }
+});
+
+export default handler;
