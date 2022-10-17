b55a00d8e1172950c7f417fe7372acb8e0acdd68
Luke Prosser <lukesprosser@gmail.com>
2022-10-17 20:39:07 +0100

    create product update endpoint

--- pages/api/admin/products/[id]/index.ts
+++ pages/api/admin/products/[id]/index.ts
@@ -15,4 +15,27 @@ handler.get(async (req, res) => {
   res.send(product);
 });
 
+handler.put(async (req, res) => {
+  await db.connect();
+  const product = await Product.findById(req.query.id);
+
+  if (product) {
+    product.name = req.body.name;
+    product.slug = req.body.slug;
+    product.brand = req.body.brand;
+    product.category = req.body.category;
+    product.image = req.body.image;
+    product.price = req.body.price;
+    product.stockCount = req.body.stockCount;
+    product.description = req.body.description;
+
+    await product.save();
+    await db.disconnect();
+    res.send({ message: 'Product updated successfully.' });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: 'Product not found.' });
+  }
+});
+
 export default handler;
--- utils/error.ts
+++ utils/error.ts
@@ -2,7 +2,9 @@ import { AxiosError } from 'axios';
 
 export const getErrorMsg = (error: unknown) => {
   if (error instanceof AxiosError) {
-    return error.response ? error.response.data.message : error.message;
+    return error?.response?.data.message
+      ? error.response.data.message
+      : error.message;
   } else {
     return 'An unexpected error occurred, please try again.';
   }
