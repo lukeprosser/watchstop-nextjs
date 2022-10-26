bbb544b7428804a9b4745f81a42dc6cb1379d140
Luke Prosser <lukesprosser@gmail.com>
2022-10-26 17:42:02 +0100

    create create product endpoint

---
+++ pages/api/admin/products/index.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+import { Types } from 'mongoose';
+import { nanoid } from 'nanoid';
+import Product from '../../../../models/Product';
+import { isAuthenticated, isAdministrator } from '../../../../utils/auth';
+import db from '../../../../utils/db';
+
+type IUser = {
+  _id: Types.ObjectId;
+};
+
+interface IGetUserAuthInfoRequest extends NextApiRequest {
+  user: IUser;
+}
+
+const handler = nc<IGetUserAuthInfoRequest, NextApiResponse>();
+
+handler.use(isAuthenticated, isAdministrator);
+
+handler.get(async (req, res) => {
+  await db.connect();
+  const products = await Product.find({});
+  await db.disconnect();
+  res.send(products);
+});
+
+handler.post(async (req, res) => {
+  await db.connect();
+  const newProduct = new Product({
+    name: 'Name',
+    slug: `product-${nanoid()}`, // slug must be unique
+    brand: 'Brand',
+    category: 'Category',
+    image:
+      'https://res.cloudinary.com/lukeprosser/image/upload/v1666801514/watchstop/placeholder.jpg',
+    price: 0,
+    stockCount: 0,
+    numReviews: 0,
+    rating: 0,
+    description: 'Description',
+  });
+
+  const product = await newProduct.save();
+  await db.disconnect();
+  res.send({ message: 'Product created successfully.', product });
+});
+
+export default handler;
