0c5e5c8ed9d3cb8793f8337ad00605ad55516d3b
Luke Prosser <lukesprosser@gmail.com>
2022-11-05 15:44:22 +0000

    add scale animation to account options menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,4 @@
-import { useEffect } from 'react';
-import { ReactElement, useState } from 'react';
+import { ReactElement, useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { deleteCookie } from 'cookies-next';
@@ -20,10 +19,12 @@ const Badge = ({ cartItems }: { cartItems: IProduct[] }) => {
 };
 
 const AccountOptions = ({
+  showAccountOptions,
   setShowAccountOptions,
   dispatch,
   admin,
 }: {
+  showAccountOptions: boolean;
   setShowAccountOptions: Function;
   dispatch: Function;
   admin: boolean;
@@ -39,7 +40,11 @@ const AccountOptions = ({
   };
 
   return (
-    <div className='cursor-pointer top-10 absolute z-10 w-32 p-4 border rounded drop-shadow-lg md:right-0 bg-slate-100 border-slate-700 md:py-2 md:text-right md:top-12 before:absolute before:bg-slate-100 before:content-[""] before:rotate-45 before:rounded-tl before:-top-2 before:left-3 before:w-4 before:h-4 before:border-t-inherit before:border-l-inherit before:md:left-[90px]'>
+    <div
+      className={`${
+        showAccountOptions ? `scale-100` : `scale-0`
+      } transition origin-[10%_top] md:origin-[75%_top] ease-in-out duration-100 bg-slate-100 cursor-pointer top-10 absolute z-10 w-32 p-4 border rounded drop-shadow-lg md:right-0 border-slate-700 md:py-2 md:text-right md:top-12 before:absolute before:bg-slate-100 before:content-[""] before:rotate-45 before:rounded-tl before:-top-2 before:left-3 before:w-4 before:h-4 before:border-t-inherit before:border-l-inherit before:md:left-[90px]`}
+    >
       <ul className='text-sm divide-y text-slate-900'>
         {admin && (
           <li className='py-2 hover:text-slate-600'>
@@ -82,7 +87,7 @@ export default function Header(): ReactElement {
 
   const accountMenuRef = useClickOutside(() => setShowAccountOptions(false));
 
-  // Workaround to prevent hydration error
+  // Workaround to prevent hydration error by forcing re-render
   useEffect(() => {
     setCartItemsExist(true);
     setUserInfoExists(true);
@@ -125,24 +130,25 @@ export default function Header(): ReactElement {
             </li>
             <li ref={accountMenuRef} className='relative'>
               {userInfoExists && userInfo ? (
-                <button
-                  type='button'
-                  className='py-2 md:p-4 hover:text-slate-50'
-                  onClick={() => setShowAccountOptions(!showAccountOptions)}
-                >
-                  {userInfo.name}
-                </button>
+                <>
+                  <button
+                    type='button'
+                    className='py-2 md:p-4 hover:text-slate-50'
+                    onClick={() => setShowAccountOptions(!showAccountOptions)}
+                  >
+                    {userInfo.name}
+                  </button>
+                  <AccountOptions
+                    showAccountOptions={showAccountOptions}
+                    setShowAccountOptions={setShowAccountOptions}
+                    dispatch={dispatch}
+                    admin={userInfo.admin}
+                  />
+                </>
               ) : (
                 <Link href='/login'>
                   <a className='block py-2 md:p-4 hover:text-slate-50'>Login</a>
                 </Link>
-              )}
-              {showAccountOptions && (
-                <AccountOptions
-                  setShowAccountOptions={setShowAccountOptions}
-                  dispatch={dispatch}
-                  admin={userInfo.admin}
-                />
               )}
             </li>
           </ul>
