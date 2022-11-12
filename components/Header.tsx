867a216315a4d732ea4d7f769b05b064ae57eb7f
Luke Prosser <lukesprosser@gmail.com>
2022-11-12 18:16:57 +0000

    update brand logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,11 +3,12 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { deleteCookie } from 'cookies-next';
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import { ClockIcon } from '@heroicons/react/24/solid';
 import useStore from '../hooks/useStore';
 import useClickOutside from '../hooks/useClickOutside';
-import { IProduct } from '../constants';
+import { IProductOrder } from '../constants';
 
-const Badge = ({ cartItems }: { cartItems: IProduct[] }) => {
+const Badge = ({ cartItems }: { cartItems: IProductOrder[] }) => {
   return (
     <span
       className='absolute w-4 h-4 text-center bg-red-600 rounded-full md:right-0 text-xxs md:top-4'
@@ -97,7 +98,10 @@ export default function Header(): ReactElement {
     <header className='text-lg text-slate-50 bg-slate-900 md:py-0'>
       <div className='container flex flex-wrap items-center justify-between px-4 py-2 mx-auto'>
         <Link href='/'>
-          <a className='text-xl font-semibold text-slate-50'>WatchStop</a>
+          <a className='text-xl font-semibold text-slate-50 flex items-center tracking-wider'>
+            WATCHST
+            <ClockIcon className='w-5 h-5' />P
+          </a>
         </Link>
         {showMobileNav ? (
           <XMarkIcon
